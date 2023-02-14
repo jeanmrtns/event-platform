a252416889ed4a69b1af37cdd8747eef1bf9570b
Jean Martins <jeanmrtns4@gmail.com>
2023-02-14 15:59:19 -0300

    feat(header): create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-import { gql, useQuery } from '@apollo/client'
-
-interface Lesson {
-  id: string
-  title: string
-  teacher: {
-    bio: string
-    name: string
-  }
-}
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        bio
-        name
-      }
-    }
-  }
-`
+import { Event } from './pages/Event'
 
 export function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
-  )
+  return <Event />
 }
