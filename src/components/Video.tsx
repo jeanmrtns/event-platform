4621b0b44c74872c3496a0c66fb15bc22f625566
Jean Martins <jeanmrtns4@gmail.com>
2023-02-16 17:43:33 -0300

    refactor(graphql): automate graphql operations
    Graphql hooks are now created automatically when creating a new query or mutation by runing "npm run
    codegen"

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,50 +1,22 @@
-import { gql, useQuery } from '@apollo/client'
 import { DefaultUi, Player, Youtube } from '@vime/react'
 import { DiscordLogo, FileArrowDown, Image, Lightning } from 'phosphor-react'
 import { Button, Card } from '@/components'
 
 import '@vime/core/themes/default.css'
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      videoId
-      title
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`
+import { useGetLessonBySlugQuery } from '@/graphql/generated'
 
 interface VideoProps {
   lessonSlug: string
 }
 
-interface GetLessonBySlugResponse {
-  lesson: {
-    videoId: string
-    title: string
-    description: string
-    teacher: {
-      bio: string
-      avatarURL: string
-      name: string
-    }
-  }
-}
-
 export function Video({ lessonSlug }: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: lessonSlug,
     },
   })
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return <p className="flex-1">Carregando</p>
   }
 
@@ -53,7 +25,7 @@ export function Video({ lessonSlug }: VideoProps) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data?.lesson.videoId!} />
+            <Youtube videoId={data.lesson.videoId!} />
             <DefaultUi />
           </Player>
         </div>
@@ -63,28 +35,30 @@ export function Video({ lessonSlug }: VideoProps) {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <strong className="text-2xl text-gray-100">
-              {data?.lesson.title}
+              {data.lesson.title}
             </strong>
 
             <p className="text-gray-200 text-base leading-6 mt-4">
-              {data?.lesson.description}
+              {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data?.lesson.teacher.avatarURL}
-                alt=""
-              />
-              <div className="flex flex-col leading-relaxed">
-                <span className="font-bold text-2xl text-gray-100">
-                  {data?.lesson.teacher.name}
-                </span>
-                <span className="text-sm text-gray-400">
-                  {data?.lesson.teacher.bio}
-                </span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt=""
+                />
+                <div className="flex flex-col leading-relaxed">
+                  <span className="font-bold text-2xl text-gray-100">
+                    {data.lesson.teacher.name}
+                  </span>
+                  <span className="text-sm text-gray-400">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
